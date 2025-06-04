18be2c9ed79b0674403502e69a0522df8d54b8ac
Bonolo Nthejane <bknthejane@gmail.com>
2025-06-04 22:55:44 +0200

    Modified the UserCard component from the demo to the correct one

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,9 +1,28 @@
-export const UserCard = () => {
-    return <>
-        <div className='card'>
-            <img></img>
-            <h2>User 1</h2>
-            <p>This is a demo</p>
-        </div>
-    </>
+import React from "react";
+import type { GitHubUser } from "../types";
+import { useNavigate } from "react-router-dom";
+
+interface Props {
+    user: GitHubUser;
+    isFavorite: boolean;
+    onToggleFavorite: () => void;
 }
+
+const UserCard: React.FC<Props> = ({ user, isFavorite, onToggleFavorite }) => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div>
+                <img src={user.avatar_url} alt={user.login} />
+                <h3>{user.login}</h3>
+                <div>
+                    <button onClick={() => navigate(`/user/${user.login}`)}>Profile</button>
+                    <button onClick={onToggleFavorite}>{isFavorite ? '❤️' : '💔'}</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default UserCard;
