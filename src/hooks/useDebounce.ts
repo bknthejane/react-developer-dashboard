24fe87a781d8970e21975029d9b35862c0c72ce5
Bonolo Nthejane <bknthejane@gmail.com>
2025-06-04 21:00:58 +0200

    Implemented SearchBar component and made use of useDebounce hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+const useDebounce = (value: string, delay: number = 500) => {
+    const [debounceValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => setDebouncedValue(value), delay);
+        return () => clearTimeout(handler);
+    }, [value, delay]);
+
+    return debounceValue;
+};
+
+export default useDebounce;
